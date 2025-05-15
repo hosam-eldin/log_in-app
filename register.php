fa19e1cc30ebd41bebfb9d5b8bf53c62b2b871dc
h.eldin <h.eldinmoh95@gmail.com>
2025-05-15 04:09:59 +0200

    complete register

---
+++ register.php
@@ -0,0 +1,75 @@
+<?php
+
+include("db.php");
+
+$error = "";
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $confirm_password = mysqli_real_escape_string($conn, $_POST['confirm_password']);
+
+    if ($password !== $confirm_password) {
+       $error = "passwords do not match";
+    }else{
+
+        $sql = "SELECT * FROM users WHERE username = '$username' LIMIT 1";
+        $result = mysqli_query($conn, $sql);
+
+        if(mysqli_num_rows($result) === 1){
+            $error = "Username already exists, please choose another one";
+        }else{
+            $password_hash = password_hash($password, PASSWORD_DEFAULT);
+
+
+            $sql = " INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password_hash')";
+            if(mysqli_query($conn, $sql)){
+                echo "DATA INSERTED";
+            }else{
+                $error =  "DATA NOT INSERTED ,ERROR: ".mysqli_error($conn);
+            }
+        }
+
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h2>REGISTER</h2>
+
+<?php if($error != ""): ?>
+
+<p style="color: red">
+    <?php echo $error; ?>
+    </p>
+
+<?php endif; ?>
+
+<form method="post" action="">
+
+    <label for="username">USERNAME:</label><br>
+    <input type="text" name="username"  placeholder="Enter your username" required><br><br>
+
+    <label for="email">EMAIL:</label><br>
+    <input type="email" name="email" required placeholder="Email address"><br><br>
+
+    <label for="password">PASSWORD:</label><br>
+     <input type="password" name="password" required  placeholder="Password"><br><br>
+
+    <label for="confirm_password">confirm password:</label><br>
+    <input type="password" name="confirm_password" required  placeholder="Confirm password"><br><br>
+
+    <input type="submit" value="register"  >
+
+</form>
+
+
+</body>
+</html>
