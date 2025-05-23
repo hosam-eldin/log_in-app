5a633e51580b1b8a331b261d598f080c573742a5
hosam-eldin <h.eldinmoh95@gmail.com>
2025-05-23 05:37:09 +0200

    login feature adjustments

--- db.php
+++ db.php
@@ -1,13 +1,17 @@
 <?php
 
-$conn = mysqli_connect("localhost", "root", "","login_app");
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "login_app";
+$conn = mysqli_connect($host, $username, $password,$dbname);
+if(!$conn){
+    die("Connection failed: ".mysqli_connect_error());
+}
 
-function check_query( $result)
-{
-
+function check_query( $result){
     if(!$result){
         return "Erorr:" . mysqli_error($conn);
-
     }
     return true;
 }
--- index.php
+++ index.php
@@ -10,8 +10,14 @@ include "partials/header.php";
                <h1>Welcome to our PHP login APP</h1>
                <p>securely login and manage your account with us</p>
                <div class="hero-buttons">
+
+                 <?php if (!is_user_logged_in()):?>
+
                    <a class="btn" href="logout.php">login</a>
                    <a class="btn" href="register.php">register</a>
+
+                   <?php endif; ?>
+
                </div>
            </div>
 
