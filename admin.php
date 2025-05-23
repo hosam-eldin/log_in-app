97e389e77a0a5e32d5e5391229ebb120a323cdaf
hosam-eldin <h.eldinmoh95@gmail.com>
2025-05-23 02:30:47 +0200

    admin-notification

--- admin.php
+++ admin.php
@@ -8,34 +8,52 @@ if(!is_user_logged_in()){
 
 $result = mysqli_query($conn, "SELECT id, username, email, reg_date FROM users");
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    if(isset($_POST["edit_user"])){
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["edit_user"])) {
         $user_id = mysqli_real_escape_string($conn, $_POST["user_id"]);
         $new_username = mysqli_real_escape_string($conn, $_POST["username"]);
         $new_email = mysqli_real_escape_string($conn, $_POST["email"]);
 
         $sql = "UPDATE users SET email  = '$new_email',username = '$new_username' WHERE id = $user_id";
         $result = mysqli_query($conn, $sql);
-        if(check_query( $result)){
+        $query_status = check_query($result);
+        if ($query_status === true) {
+            $_SESSION['message'] = "user updated successfully";
+            $_SESSION['msg_type'] = "success";
             redirect("admin.php");
         }
 
-    }elseif (isset($_POST["delete_user"])){
+    } elseif (isset($_POST["delete_user"])) {
         $user_id = mysqli_real_escape_string($conn, $_POST["user_id"]);
         $sql = " DELETE FROM users WHERE id = $user_id";
         $result = mysqli_query($conn, $sql);
-        if(check_query($result)){
+        $query_status = check_query($result);
+        if ($query_status === true) {
+            $_SESSION['message'] = "user deleted successfully";
+            $_SESSION['msg_type'] = "success";
             redirect("admin.php");
+
         }
-}
+    }
 }
 ?>
 
 
 
     <h1>Manage Users</h1>
-
     <div class="container">
+
+        <?php if(isset($_SESSION["message"])):?>
+
+            <div class="notification  <?php echo $_SESSION['msg_type'];?>" >
+                <?php echo $_SESSION["message"];?>
+                <?php unset($_SESSION["message"]);?>
+                <?php unset($_SESSION["msg_type"]);?>
+            </div>
+
+        <?php endif;?>
+
+
         <table class="user-table">
             <thead>
             <tr>
